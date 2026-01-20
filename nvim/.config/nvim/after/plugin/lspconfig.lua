f06a201cf8d6a71083d99ae9e4ebe23c1a1440d9
Dan Billings <danbills@live.com>
2026-01-19 20:37:47 -0500

    update

--- nvim/.config/nvim/after/plugin/lspconfig.lua
+++ nvim/.config/nvim/after/plugin/lspconfig.lua
@@ -1,8 +1,8 @@
-require'lspconfig'.pyright.setup{}
-
-require'lspconfig'.terraformls.setup{}
-require'lspconfig'.tflint.setup{}
-require'lspconfig'.clangd.setup{}
+-- Enable LSP servers using the new vim.lsp API (nvim 0.11+)
+vim.lsp.enable('pyright')
+vim.lsp.enable('terraformls')
+vim.lsp.enable('tflint')
+vim.lsp.enable('clangd')
 
 vim.cmd([[silent! autocmd! filetypedetect BufRead,BufNewFile *.tf]])
 vim.cmd([[autocmd BufRead,BufNewFile *.hcl set filetype=hcl]])
@@ -10,10 +10,9 @@ vim.cmd([[autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl]
 vim.cmd([[autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform]])
 vim.cmd([[autocmd BufRead,BufNewFile *.tfstate,*.tfstate.backup set filetype=json]])
 
-
-require'lspconfig'.groovyls.setup{
-    -- Unix
+-- Groovy LSP with custom config
+vim.lsp.config.groovyls = {
     cmd = { "java", "-jar", "/Users/dbillings/groovy-language-server/build/libs/groovy-language-server-all.jar" },
-    ...
 }
+vim.lsp.enable('groovyls')
 
